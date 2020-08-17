44f95a4d1d19ad68c53c5220b3646b543d5b1064
Revanth <revanth.ms@pratian.com>
2020-08-17 12:56:52 +0530

    Add Remove Ingredients

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
+import classes from "./BuildControls.css";
+import BuildControl from "./BuildControl/BuildControl";
+
+const controls = [
+  { label: "Salad", type: "Salad" },
+  { label: "Bacon", type: "Bacon" },
+  { label: "Cheese", type: "Cheese" },
+  { label: "Meat", type: "Meat" },
+];
 
 const BuildControls = (props) => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+  return (
+    <div className={classes.BuilldControls}>
+      {controls.map((control) => {
+        return (
+          <BuildControl
+            key={control.label}
+            label={control.label}
+            added={()=>{props.ingredientsAdded(control.type)}}
+            removed={()=>{props.ingredientsRemoved(control.type)}}
+          />
+        );
+      })}
+    </div>
+  );
+};
 
-export default BuildControls
+export default BuildControls;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,27 +1,85 @@
-import React, { Component } from 'react'
-import Aux from '../../hoc/Aux'
-import Burger from '../../components/Burger/Burger'
+import React, { Component } from "react";
+import Aux from "../../hoc/Aux";
+import Burger from "../../components/Burger/Burger";
+import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 
+const INGREDIENTS_PRICE = {
+  Salad: 0.4,
+  Meat: 1.3,
+  Cheese: 0.7,
+  Bacon: 0.1,
+};
 class BurgerBuilder extends Component {
-    constructor(props){
-        super(props)
-        this.state ={
-            ingredients :{
-                Salad : 1,
-                Bacon :1,
-                Cheese :0,
-                Meat : 1    
-            }
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      ingredients: {
+        Salad: 1,
+        Bacon: 1,
+        Cheese: 0,
+        Meat: 1,
+      },
+      totalPrice: 4,
+    };
+  }
+
+  addIngredientsHandler(type) {
+    //Update the ingredients count
+    console.log(this.state);
+    const addCount = this.state.ingredients[type] + 1;
+    const updatedIngredients = {
+      ...this.state.ingredients,
+    };
+    updatedIngredients[type] = addCount;
+
+    //Update the price
+    const oldPrice = this.state.totalPrice;
+    const addPrice = INGREDIENTS_PRICE[type];
+    const currentPrice = oldPrice + addPrice;
+    this.setState({
+      ingredients: updatedIngredients,
+      totalPrice: currentPrice,
+    });
+  }
+  removeIngredientHandler(type) {
+    //Update the ingredients count
+    console.log(this.state);
+    const removeCount = this.state.ingredients[type] - 1;
+    if(removeCount<0){
+        return
     }
-    render() {
-        return (
-           <Aux>
-               <Burger ingredients={this.state.ingredients} />
-               <div>Builder controls</div>
-           </Aux>
-        )
-    }
+    const updatedIngredients = {
+      ...this.state.ingredients,
+    };
+    updatedIngredients[type] = removeCount;
+
+    //Update the price
+    const oldPrice = this.state.totalPrice;
+    const removePrice = INGREDIENTS_PRICE[type];
+    const currentPrice = oldPrice + removePrice;
+    this.setState({
+      ingredients: updatedIngredients,
+      totalPrice: currentPrice,
+    });
+  }
+
+  render() {
+      let disabledInfo = {
+          ...this.state.ingredients
+      }
+      for (let key in disabledInfo){
+          disabledInfo[key] = disabledInfo[key]<=0? true : false
+      }
+    return (
+      <Aux>
+        <Burger ingredients={this.state.ingredients} />
+        <BuildControls
+          ingredientsAdded={this.addIngredientsHandler.bind(this)}
+          ingredientsRemoved={this.removeIngredientHandler.bind(this)}
+        />
+      </Aux>
+    );
+  }
 }
 
 export default BurgerBuilder;
